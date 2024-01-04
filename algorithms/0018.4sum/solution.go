85d9908ecba72b563d2db60ae981c0113c7fa68f
chenjiacheng <405339556@qq.com>
2024-01-04 23:45:26 +0800

    18 accepted. 8ms, faster than 77.40%

--- algorithms/0018.4sum/solution.go
+++ algorithms/0018.4sum/solution.go
@@ -51,38 +51,19 @@ func fourSum(nums []int, target int) [][]int {
 	return ans
 }
 
-func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
-	mp := make(map[int]int)
-	for _, v1 := range nums1 {
-		for _, v2 := range nums2 {
-			mp[v1+v2]++
-		}
-	}
-
-	ans := 0
-	for _, v3 := range nums3 {
-		for _, v4 := range nums4 {
-			ans += mp[-v3-v4]
-		}
-	}
-	return ans
-}
-
 type Example struct {
-	nums1 []int
-	nums2 []int
-	nums3 []int
-	nums4 []int
-	ans   int
+	nums   []int
+	target int
+	ans    [][]int
 }
 
 func main() {
 	examples := []Example{
-		{[]int{1, 2}, []int{-2, -1}, []int{-1, 2}, []int{0, 2}, 2},
-		{[]int{0}, []int{0}, []int{0}, []int{0}, 1},
+		{[]int{1, 0, -1, 0, -2, 2}, 0, [][]int{{-2, -1, 1, 2}, {-2, 0, 0, 2}, {-1, 0, 0, 1}}},
+		{[]int{2, 2, 2, 2, 2}, 8, [][]int{{2, 2, 2, 2}}},
 	}
 	for i, e := range examples {
-		ans := fourSumCount(e.nums1, e.nums2, e.nums3, e.nums4)
+		ans := fourSum(e.nums, e.target)
 		if reflect.DeepEqual(ans, e.ans) {
 			fmt.Println("PASS: CASE", i)
 		} else {
