fce2ade21c326dd57dd7b688bfd11a7602bbcbf7
chenjiacheng <405339556@qq.com>
2024-01-17 22:21:43 +0800

    104 accepted. 0ms, faster than 100.00%

---
+++ algorithms/0104.maximum-depth-of-binary-tree/solution.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type Example struct {
+	root *TreeNode
+	ans  int
+}
+
+func main() {
+	examples := []Example{
+		{&TreeNode{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}}, 3},
+		{&TreeNode{Val: 1, Right: &TreeNode{Val: 2}}, 2},
+	}
+	for i, e := range examples {
+		ans := maxDepth(e.root)
+		if reflect.DeepEqual(ans, e.ans) {
+			fmt.Println("PASS: CASE", i)
+		} else {
+			fmt.Println("FAIL: CASE", i)
+		}
+	}
+}
